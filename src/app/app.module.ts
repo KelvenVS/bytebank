9f8f285fad6f052ce8ddc9afcb4872594ab92db9
KelvenVS <kelven.serejo@gmail.com>
2023-01-31 23:12:16 -0300

    Setando data e localização

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { newTransferComponent } from './new-transfer/new-transfer.component';
-import { NgModule } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { LOCALE_ID } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { ExtractComponent } from './extract/extract.component';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+
+
+registerLocaleData(localePt, 'pt');
 
 @NgModule({
   declarations: [
@@ -16,7 +21,12 @@ import { ExtractComponent } from './extract/extract.component';
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [
+    {provide: LOCALE_ID, useValue: 'pt' },
+      {provide: DEFAULT_CURRENCY_CODE,
+        useValue: 'BRL',
+      },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
