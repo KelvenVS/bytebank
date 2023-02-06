1e1981afdbc7544e10145d9d0d9414abd9882fdb
KelvenVS <kelven.serejo@gmail.com>
2023-02-06 17:19:28 -0300

    importando e exportando router

--- src/app/services/app-routing.module.ts
+++ src/app/services/app-routing.module.ts
@@ -1,8 +1,17 @@
-import { NgModule } from '@angular/core';
+import { newTransferComponent } from './../new-transfer/new-transfer.component';
+import { ExtractComponent } from './../extract/extract.component';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {path: '', redirectTo: 'extract', pathMatch: 'full'},
+  {path: 'extract', component: ExtractComponent},
+  {path: 'new-transfer', component: newTransferComponent}
+];
 
 @NgModule({
-  imports: [RouterModule],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 
